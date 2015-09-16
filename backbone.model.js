3b4e1d3352fe2a7e861da01283a60669b7c280a1
silentmood <silentmood198909@gmail.com>
2015-09-16 12:08:04 +0800

    fixed the dep related code

--- backbone.model.js
+++ backbone.model.js
@@ -12,11 +12,19 @@
   var root = (typeof self == 'object' && self.self == self && self) ||
             (typeof global == 'object' && global.global == global && global);
 
-  root.Backbone = factory(
-    root, 
-    (typeof exports !== 'undefined')? exports : {}, 
-    ((typeof define === 'function' && define.amd) || typeof exports !== 'undefined')? require('underscore') : root._, 
-    (root.jQuery || root.Zepto || root.ender || root.$));
+  var $ = (root.jQuery || root.Zepto || root.ender || root.$);
+  // Set up Backbone appropriately for the environment. Start with AMD.
+  if (typeof define === 'function' && define.amd) {
+    // Export global even in AMD case in case this script is loaded with
+    // others that may still expect a global Backbone.
+    root.Backbone = factory(root, require('exports'), require('underscore'), $);
+  // Next for Node.js or CommonJS. jQuery may not be needed as a module.
+  } else if (typeof exports !== 'undefined') {
+    factory(root, exports, require('underscore'), $);
+  // Finally, as a browser global.
+  } else {
+    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
+  }
 }(function(root, Backbone, _, $) {
 
   // Initial Setup
